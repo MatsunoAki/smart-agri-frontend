7c4554b119c9ec635a1b50d841c1f6f1d45f71ae
MatsunoAki <rivera.sambile@gmail.com>
2025-02-03 18:59:21 +0800

    Added dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import './App.css'
 import Home from './pages/Home'
 import Navbar from './Components/Navbar'
 import Login from './pages/Auth/login'
 import Register from './pages/Auth/register'
+import Dashboard from './pages/dashboard/Dashboard';
 import { useState } from 'react'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
