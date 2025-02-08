b1bff1f233f91a1318b536ed5c0cd10c03639012
MatsunoAki <rivera.sambile@gmail.com>
2025-02-08 19:23:16 +0800

    Added Auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import './App.css'
+import { AuthProvider } from './contexts/AuthContext';
 import Home from './pages/Home'
 import Navbar from './Components/Navbar'
 import Login from './pages/Auth/login'
 import Register from './pages/Auth/register'
 import Dashboard from './pages/dashboard/Dashboard';
+import Devices from './pages/dashboard/Devices';
 import { useState } from 'react'
 
 function App() {
   return (
+
     <Router>
-      <div className="App">
+    <AuthProvider>
+    <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard/home" element={<Dashboard />} />
+          <Route path="/dashboard/devices" element={<Devices />} />
         </Routes>
-      </div>
+      </div>    </AuthProvider>
     </Router>
   );
 }
