19fc2c70b91eca3ea26249fd9afe5faa7399be54
MatsunoAki <rivera.sambile@gmail.com>
2025-02-08 19:22:25 +0800

    Set configs for firebase

---
+++ firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCiCadn4yWKCEeWGCMgMFRXf8Ka8kgGT4s",
+
+    authDomain: "smart-agri-irrigation.firebaseapp.com",
+
+    databaseURL: "https://smart-agri-irrigation-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+    projectId: "smart-agri-irrigation",
+
+    storageBucket: "smart-agri-irrigation.firebasestorage.app",
+
+    messagingSenderId: "496188879553",
+
+    appId: "1:496188879553:web:060a27c295694adc3a5a40",
+
+    measurementId: "G-HRG4SNCV1J"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';  // Import path module
+import withMT from "@material-tailwind/react/utils/withMT";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(withMT({
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),  // Alias for 'src' folder
+    },
+  },
+}));
