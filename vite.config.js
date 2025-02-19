3535fb563235890c686d50400e591ba54bd9784f
MatsunoAki <rivera.sambile@gmail.com>
2025-02-19 20:50:01 +0800

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';  // Import path module
+import path from 'path';
 import withMT from "@material-tailwind/react/utils/withMT";
 
-// https://vite.dev/config/
 export default defineConfig(withMT({
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),  // Alias for 'src' folder
+    theme: {
+        extend: {},
     },
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+      proxy: {
+          '/api': {
+              target: 'http://localhost:3001', // Correct URL and port!
+              changeOrigin: true,
+          },
+      },
   },
 }));
