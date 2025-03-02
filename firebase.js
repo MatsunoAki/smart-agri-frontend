d574fd63971e6bfacf0958fc0a83d65b11d75f75
MatsunoAki <rivera.sambile@gmail.com>
2025-03-02 18:21:12 +0800

    added db for firestore

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
 
@@ -26,4 +27,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const database = getDatabase(app);
